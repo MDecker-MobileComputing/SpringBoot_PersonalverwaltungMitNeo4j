fe043b94624e109b532c6d2ae1cd449e2788b585
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-05-31 20:56:27 +0200

    Projekt gerade neue mit Spring Initializr erzeugt https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.5.0&packaging=jar&jvmVersion=21&groupId=de.eldecker.spring&artifactId=personaldb&name=Personaldatenbank&description=Personaldatenbank%20mit%20Neo4j&packageName=de.eldecker.spring.personaldb&dependencies=thymeleaf,data-neo4j

---
+++ src/test/java/de/eldecker/spring/personaldb/PersonaldatenbankApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.spring.personaldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaldatenbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
